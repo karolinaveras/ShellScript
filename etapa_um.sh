a47dff791dd8c134d3bb143d9f0fd28289f8337f
Maria Karolina Rodrigues Veras <mariakarolina_veras@hotmail.com>
2018-10-15 13:26:06 -0300

    Create etapa_um.sh

---
+++ etapa_um.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MY_DOMAIN="mydomain.local"
+
+GET_ARCH=$(getconf LONG_BIT)
+
+PBIS_OPEN="https://github.com/BeyondTrust/pbis-open/releases/download/8.7.0/pbis-open-8.7.0.478.linux.x86_64.deb.sh"
+PBIS_FILE="https://github.com/BeyondTrust/pbis-open/releases/download/8.7.0/pbis-open-8.7.0.478.linux.x86.deb.sh"
+
